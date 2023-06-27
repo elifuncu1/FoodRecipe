e4db7e6c93a50d676d76e1042d97fb6c68260c49
Melik Fırat Başçi <70327175+MelikFiratBasci@users.noreply.github.com>
2023-06-27 18:49:00 +0300

    public folder added

--- src/controllers/BusinessModules/ProductModules/priceCalculateModule.js
+++ src/controllers/BusinessModules/ProductModules/priceCalculateModule.js
@@ -1,82 +1,98 @@
 const jcc = require('json-case-convertor');
-//queryden sadece json _doc gelmeli!
-module.exports.getCostOfProduct = function (productList,wantedQuantity,x){//birim litre veya Kg ise (1000) "alt tipe dönüşüm"
-    //
+
+// Verilen bir ürün listesi, istenen miktar ve dönüşüm faktörü kullanarak ürün fiyatlarını hesaplar
+module.exports.getCostOfProduct = function (productList, wantedQuantity, x) {
     var index = 0; 
     var listForReturn = productList ;
     var factor;
     var productQuantity;
-    var unit
+    var unit;
     var cheapestPrice = Number.MAX_SAFE_INTEGER;
     var cheapestProduct;
     var avgPrice = 0;
-    try{
+    
+    try {
         productList.forEach(element => {
-        productQuantity = findQuantityOfProducts(element);
-        unit = findUnit(element);
-        if(unit == "GR" || unit == "ML" || unit == "G"){
-            factor = wantedQuantity * x / productQuantity 
-        }else if(unit == "KG" || unit == "LT" || unit == "L"){//adet için vs sonra ayarlama yaparız...
-            factor = wantedQuantity * x / productQuantity / 1000 
-        }
-        else{
-            factor = wantedQuantity * x / productQuantity 
-        }
-        listForReturn[index].requested_price = element.product_price * factor ; 
-        avgPrice += listForReturn[index].requested_price;
-        if(element.product_price < cheapestPrice){
-            cheapestProduct = element
-        }
-        index ++ ;
-    });
-}
-catch{
-    return 0
-}
-    avgPrice = avgPrice / index;
-    return {'productList' :listForReturn,'cheapestProduct' : cheapestProduct,'avaragePrice' : avgPrice};
+            productQuantity = findQuantityOfProducts(element); // Her eleman için ürün miktarını bulur
+            unit = findUnit(element); // Her eleman için ürün birimini bulur
+            
+            // Birim GR, ML veya G ise (1000) "alt tipe dönüşüm" yapılır
+            if (unit == "GR" || unit == "ML" || unit == "G") {
+                factor = wantedQuantity * x / productQuantity;
+            } 
+            // Birim KG, LT veya L ise (1000) ile çarparak "alt tipe dönüşüm" yapılır
+            else if (unit == "KG" || unit == "LT" || unit == "L") {
+                factor = wantedQuantity * x / productQuantity / 1000;
+            }
+            else {
+                factor = wantedQuantity * x / productQuantity;
+            }
+            
+            listForReturn[index].requested_price = element.product_price * factor;
+            avgPrice += listForReturn[index].requested_price;
+            
+            if (element.product_price < cheapestPrice) {
+                cheapestProduct = element;
+            }
+            
+            index++;
+        });
+        avgPrice = avgPrice / index;
     
-}
- function findQuantityOfProducts (product){
-        
-        return product.product_name.replace(/\D/g, '');
-    
-}
- function findUnit (product){//artık birim ilk üründen rahat bir şekilde gözükebiliyor h.o //Katı sıvı için! 
-  return  product.product_name.slice(-2);
-}
-module.exports.forToUpperCase = function (productList){
-    var listForReturn = productList; 
-    var index = 0;
-    try{productList.forEach(element => {
-        listForReturn[index] = jcc.upperCaseValues(element);
-        index++ ;
-    });
-    }catch(err){
+        return {
+            'productList': listForReturn,
+            'cheapestProduct': cheapestProduct,
+            'averagePrice': avgPrice
+        };
+    } catch(err) {
         console.log(err)
     }
+    
+  
+}
+
+// Ürün listesindeki her bir ürünün değerlerini büyük harfe dönüştürür
+module.exports.forToUpperCase = function (productList) {
+    var listForReturn = productList; 
+    var index = 0;
+    
+    try {
+        productList.forEach(element => {
+            listForReturn[index] = jcc.upperCaseValues(element);
+            index++;
+        });
+    } catch(err) {
+        console.log(err);
+    }
+    
     return listForReturn;
 }
-module.exports.findQuantityOfProducts= function(product){//sadece export için geçici bir yazım daha düzgün gösterim varsa düzeltilmeli
-    return findQuantityOfProducts(product);
+
+// Ürün adından ürün miktarını bulur
+function findQuantityOfProducts(product) {
+    return product.product_name.replace(/\D/g, '');
 }
-module.exports.findUnit = function(product){
-    return findUnit(product);
+
+// Ürün adından ürün birimini bulur
+function findUnit(product) {
+    return product.product_name.slice(-2);
 }
-module.exports.calculateTotalValueOfRecipe = function(selectedProducts){
+
+// Seçilen ürünlerin toplam değerini hesaplar
+module.exports.calculateTotalValueOfRecipe = function(selectedProducts) {
     var totalPrice = 0;
-    try{selectedProducts.forEach(element => {
-        try{
-        totalPrice += element.requested_price;
-        }
-        catch{
-            
-        }
-    });
-    }catch(err){
-        console.log(err)
+    
+    try {
+        selectedProducts.forEach(element => {
+            try {
+                totalPrice += element.requested_price;
+            } catch {
+                
+            }
+        });
+    } catch(err) {
+        console.log(err);
     }
 
     return totalPrice;
 }
-
--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -4,33 +4,46 @@ const Products = require('../models/products');
 const FoodIngredients = require('../models/foodIngredients');
 const foodRecipes = require('../models/foodRecipe');
 const User = require('../models/userModel');
+const priceCalculateModule = require("./BusinessModules/ProductModules/priceCalculateModule");
 
+const homePage = async (req, res, next) => {
+    try {
+        res.render('home/homePage', { layout: '../layouts/Home/homeLayout', title: 'Yemek Tarifleri', description: '', keywords: '' });
+    } catch (err) {
+        console.log(err);
+    }
+};
 
-const GetPrice = async (req,res,next) => {
-    try{
-        var ProductsList = []
-        const findRecipes = await foodRecipes.findOne({Recipe_Name: req.params.recipeName})
-        
-        const ProductList = findRecipes.Recipe_Ingredients.split(',')
-        for(let i = 0;i<ProductList.length;i++){
-            const words = ProductList[i].split(':')[0].split(' ')
+const GetPrice = async (req, res, next) => {
+    try {
+        const ProductsList = [];
+        const findedRecipe = await foodRecipes.findOne({ Recipe_Name: req.params.recipeName });
+        const ProductList = findedRecipe.Recipe_Ingredients.split(',');
+        let IngredientsList = [];
+        for (let i = 0; i < ProductList.length; i++) {
+            const words = ProductList[i].split(':')[0].split(' ');
             const lastWord = words[words.length - 1];
-            const FoodFind = await Products.find({product_name:  { "$regex": lastWord, $options: 'i' }})
-            var info = {
-
+            const FoodFind = await Products.find({ product_name: { $regex: lastWord, $options: 'i' } });
+            const info = {
                 FoodName: ProductList[i].split(':')[0],
                 productList: FoodFind,
                 wantedQuantity: ProductList[i].split(':')[2],
                 x: ProductList[i].split(':')[1]
-            }
-            ProductsList.push(info)     
+            };
+            IngredientsList[i] = await FoodIngredients.findOne({Ingredients_Name : info.FoodName});
+
+
+            ProductsList.push(await priceCalculateModule.getCostOfProduct(info.productList, info.wantedQuantity, info.x));
         }
-        res.json(ProductsList)
+        console.log(ProductsList)
+        res.render('home/showRecipePage', { layout: '../layouts/Home/homeLayout', title: 'Yemek Tarifleri', description: '', keywords: '', findedRecipe, IngredientsList ,ProductsList});
+    } catch (err) {
+        console.log(err);
     }
-    catch (err){
-        console.log(err)
-    }
-}
+};
+
+
 module.exports = {
-    GetPrice
-}
+    GetPrice,
+    homePage
+};
